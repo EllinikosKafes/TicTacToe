fc4e680fe5ab004523409c0e83020a909355297b
Nikitas Stefanidis <61457804+EllinikosKafes@users.noreply.github.com>
2025-10-26 16:49:50 +0200

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,44 @@
-from game import game_manager
-from ui import renderer
+from game import game_manager,player,board
+from ui import renderer,events
 import pygame
 
+
+def main_game():
+
+    player1 = player.Player("Me",'x')
+    player2 = player.Player("John","o")
+    current_player = player1
+    board.clear()
+    running = True
+    while running:
+        
+        number = events.check_box()
+        if number:
+            if not board.check_if_pressed(number):
+                board.press_square(number,current_player.symbol)
+                win=board.check_win()
+                if win == True:
+                    print(f"{current_player.name} wins!")
+
+                    renderer.refresh(clock, screen, board.get_board())
+                    pygame.display.update()
+
+                    action = game_manager.show_end_screen(screen, clock, current_player.name)
+                    return action
+                    running = False
+                    
+                current_player = player2 if current_player==player1 else player1
+        
+        renderer.refresh(clock,screen,board.get_board())
+
 pygame.init()
-clock,screen = renderer.setting_up(400,400)
-running = True
+clock, screen = renderer.setting_up(400, 400)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
+mode = game_manager.menu(screen)
 
-
-    renderer.refresh(clock,screen)
+while True:
+    action = main_game()
+    if action == "quit":
+        break
 
 pygame.quit()
